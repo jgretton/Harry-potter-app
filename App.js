7d9d13d319035873354c60d8e925527530281131
Joshua Gretton <jb.gretton@googlemail.com>
2022-06-24 20:54:10 +0100

    basic display for API fetch

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./Screens/Home";
+import ModalScreen from "./Screens/Modal";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -11,7 +10,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Group screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Group>
+        <Stack.Group screenOptions={{ presentation: "modal" }}>
+          <Stack.Screen name="MyModal" component={ModalScreen} />
+        </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,64 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  Button,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
-function Home() {
-  return (
-    <View>
-      <Text>Harry Potter home page.</Text>
+import { fetchCharacters } from "../api/api";
+
+function Home({ navigation }) {
+  const [apiData, setApiData] = useState();
+
+  const data_chatacters = async () => {
+    const data = await fetchCharacters();
+
+    const filter_data = data.slice(0, 10);
+
+    setApiData(filter_data);
+  };
+
+  useEffect(() => {
+    data_chatacters();
+  }, []);
+
+  const Item = ({ name, image }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{name}</Text>
+      <Image source={{ uri: `${image}` }} style={{ width: 50, height: 50 }} />
     </View>
   );
+
+  const renderItem = ({ item }) => <Item name={item.name} image={item.image} />;
+
+  return (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <Text> Hello</Text>
+
+      <FlatList data={apiData} renderItem={renderItem} />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default Home;
