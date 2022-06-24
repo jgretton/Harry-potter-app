1a86fe3b80cb947434381d42cf97f76209e525f0
Joshua Gretton <jb.gretton@googlemail.com>
2022-06-24 17:52:15 +0100

    implemented navigation.

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./Screens/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Harry potter app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ Screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Home() {
+  return (
+    <View>
+      <Text>Harry Potter home page.</Text>
+    </View>
+  );
+}
+
+export default Home;
